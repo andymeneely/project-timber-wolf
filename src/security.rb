6226a39862414d4c2c92bffa4c58d7319f8f984b
Andy Meneely <andy@se.rit.edu>
2016-03-28 22:53:00 -0400

    ready to print security tokens

--- src/security.rb
+++ src/security.rb
@@ -3,8 +3,14 @@ require 'squib'
 data = Squib.csv file: 'data/security.csv'
 
 Squib::Deck.new(width: 300, height: 300, cards: data['Name'].size) do
+  use_layout file: 'layouts/security.yml'
   background color: :white
+  rect layout: :cut
 
+  text layout: data['Name']
 
-  save_pdf prefix: 'security_'
+  save_sheet prefix: 'security_sheet_', trim: '0.125in'
+  save_pdf file: 'security.pdf', trim: '0.125in'
+  save_png prefix: 'security_'
+
 end
