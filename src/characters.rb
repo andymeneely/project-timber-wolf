b07a29b645c7f3fd934809383eb1c22060899ee0
Andy Meneely <andy@se.rit.edu>
2016-04-03 00:46:27 -0400

    characters: new abilities, stats on cards

--- src/characters.rb
+++ src/characters.rb
@@ -11,18 +11,26 @@ Squib::Deck.new(cards: total) do
   rect layout: :cut
 
   text str: data['Name'], layout: :title
+  text layout: :Level, str: data['Level'].map { |i| "Level #{i}"}
 
   text layout: data['Skill1']
+  text layout: :Skill1Exp, str: data['Skill1Exp'].map { |i| "#{i}💡"}
   rect layout: data['Skill1']
 
   text layout: data['Skill2']
+  text layout: :Skill2Exp, str: data['Skill2Exp'].map { |i| "#{i}💡"}
   rect layout: data['Skill2']
 
   text layout: data['Skill3']
+  text layout: :Skill3Exp, str: data['Skill3Exp'].map { |i| "#{i}💡"}
   rect layout: data['Skill3']
 
   text layout: data['Skill4']
+  text layout: :Skill4Exp, str: data['Skill4Exp'].map { |i| "#{i}💡"}
   rect layout: data['Skill4']
+
+  text layout: :Memory, str: data['Memory'].map { |i| "Memory: #{i}" }
+  text layout: :Special, str: data['Special']
 
   save_png prefix: 'character_'
   save_sheet prefix: 'character_sheet_', trim: '0.125in'
