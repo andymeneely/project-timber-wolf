e5c7f7935158ef0a78e40c35942cfad27356fe33
Andy Meneely <andy@se.rit.edu>
2016-08-08 21:46:13 -0400

    tgc prototype: bullseye! 🎯

--- src/chits.rb
+++ src/chits.rb
@@ -1,18 +1,19 @@
 require 'squib'
 require_relative 'save_custom_sheet'
 
-###############################
-# Based on TGC's Medium Chits #
-###############################
+#################################
+## Based on TGC's Medium Chits ##
+#################################
+puts "Building medium chits..."
 data = Squib.xlsx file: 'data/security_tokens.xlsx', explode: ''
 Squib::Deck.new(width: 300, height: 300, cards: data['Name'].size) do
   use_layout file: 'layouts/tgc_medium_circle_chit.yml'
   background color: :white
 
   build :color do
-    background color: '#42290d'
+    background color: '#42290d' # dark brown
     svgdata = data['Image'].map do |svg|
-      File.read("img/#{svg}").gsub('fill:#000000', 'fill:#d8c586')
+      File.read("img/#{svg}").gsub('fill:#000000', 'fill:#d8c586')  # tan color
     end
     svg data: svgdata, width: 225, height: 225, x: 37.5, y: 37.5
   end
@@ -25,7 +26,19 @@ Squib::Deck.new(width: 300, height: 300, cards: data['Name'].size) do
   # save_avery_5408
 
   save_png prefix: 'security_token_', count_format: data['File']
+end
 
+###################################
+## Based on TGC's Bullseye Chits ##
+###################################
+puts "Building bullseye chits..."
+Squib::Deck.new(width: 375, height: 900, cards: 4) do
+  cyan = '#40bfbd'; purple = '#bf40ba'; yellow = '#c8cc31'; gray = '#888888'
+  background color: [cyan, purple, yellow, gray]
+  rect fill_color: '#42290d', width: width, height: 200
+  svg file: 'jewel-chit.svg'
+  # png file: 'Bullseye-Chit.png', alpha: 0.5
+  save_png prefix: 'pressure_token_', count_format: '%02d_[all]'
 end
 #
 # data = Squib.csv file: 'data/square-chits.csv'
