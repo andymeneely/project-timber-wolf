628b2e399b3daceb08ec814a8e69a95dbf5ad815
Andy Meneely <andy@se.rit.edu>
2017-01-08 21:27:17 -0500

    events: bigger cards, stories added

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 gem 'gobject-introspection'
 gem 'gio2'
-gem 'squib', '0.11.0'
+gem 'squib', '0.13.1'
 gem 'guard'
 gem 'guard-rake'
 gem 'launchy'
--- src/events.rb
+++ src/events.rb
@@ -6,7 +6,9 @@ data = Squib.xlsx(file: 'data/data.xlsx', sheet: 2) do |col, item|
   newlineate(col, item)
 end
 
-Squib::Deck.new(width: 600, height: 825, cards: data['Name'].size) do
+File.open('data/events.txt', 'w+') { |f| f.write data.to_pretty_text }
+
+Squib::Deck.new(cards: data['Name'].size) do
   use_layout file: 'layouts/events.yml'
   background color: :white
 
@@ -18,6 +20,7 @@ Squib::Deck.new(width: 600, height: 825, cards: data['Name'].size) do
   text layout: :Name,        str: data['Name']
   ellipse layout: :Order, fill_color: :black
   text layout: :Order,       str: data['Order']
+  text layout: :Story,       str: data['Story']
   text layout: :Description, str: data['Description']
 
   svg  layout: :Img, file: data['Img']
