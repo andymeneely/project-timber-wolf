020e1beab1f42e2e451afd4439bad1c22ea390a9
Andy Meneely <andy@se.rit.edu>
2017-01-09 23:49:13 -0500

    new noise trackers

--- src/helps.rb
+++ src/helps.rb
@@ -7,7 +7,6 @@ str = data['Icon'].zip(data['Description']).inject("") do |s, r|
   s + "#{r[0]} #{r[1]}\n"
 end
 
-
 Squib::Deck.new(cards: 4, width: 1125, height: 825) do
   use_layout file: 'layouts/helps.yml'
   background color: :white
@@ -17,4 +16,15 @@ Squib::Deck.new(cards: 4, width: 1125, height: 825) do
 
   save_png prefix: 'helps_'
   save_pdf file: 'helps.pdf', trim: '0.125in'
+end
+
+Squib::Deck.new(cards: 2, width: 1725, height: 1125) do
+  use_layout file: 'layouts/helps.yml'
+  background color: :white
+
+  svg file: ['noise_tracker_3p_card.svg',
+             'noise_tracker_4p_card.svg']
+
+  save_pdf file: 'noise_tracker.pdf'
+
 end
