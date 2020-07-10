c43636954bbce357ef69c1096a494bb2c515851e
Andy Meneely <andy@se.rit.edu>
2020-07-09 23:28:46 -0400

    revise with new emoji embeds

--- src/skill_backs.rb
+++ src/skill_backs.rb
@@ -4,8 +4,8 @@ require 'pp'
 require_relative 'util/helpers'
 using Squib::ArrayExtras
 
-data = Squib.xlsx(file: 'data/data.xlsx', sheet: 1) do |col, item|
-  newlineate(col, item)
+data = Squib.xlsx(file: 'data/data.xlsx', sheet: 1, explode: 'Qty') do |col, item|
+  escape_emojis(newlineate(col, item))
 end
 total = data['Name'].size
 
@@ -16,12 +16,14 @@ Squib::Deck.new(cards: total, width: 1125, height: 825) do
 
   svg file: 'skill_back.svg'
 
+  only_lvl1_2 = data['Level'].map.with_index { |x,i| [1].include?(x.to_i) ? i : nil }.compact
+
   build :color do
     png file: 'cork.png'
-    svg file: 'skill-back-color.svg'
+    svg file: 'skill-back-color.svg', range: only_lvl1_2
   end
 
-  text str: data['Name'], layout: :name
+  text str: data['Name'], layout: :name, range: only_lvl1_2
   rolls1 = data['Rolls'].zip(data['Upgrade1Rolls']).map do |(amateur,pro)|
     arr = amateur.to_s.split('/').zip(pro.to_s.split('/')).map do | (am_act, pro_act) |
       if am_act == pro_act
@@ -48,13 +50,17 @@ Squib::Deck.new(cards: total, width: 1125, height: 825) do
     arr.join
   end
 
-  text str: data['Upgrade1'], layout: :upgrade1
-  text str: data['Upgrade1'].map { |s| summarize_skill(s) }, layout: :up1_sum
-  text str: rolls1, layout: :up1_sum, markup: true
+  text str: data['Upgrade1'], layout: :upgrade1, range: only_lvl1_2
+  text str: data['Upgrade1'].map { |s| summarize_skill(s) }, layout: :up1_sum, range: only_lvl1_2
+  text str: rolls1, layout: :up1_sum, markup: true, range: only_lvl1_2 do |embed|
+    embed_emojis(embed, 30)
+  end
 
-  text str: data['Upgrade2'], layout: :upgrade2
-  text str: data['Upgrade2'].map { |s| summarize_skill(s) }, layout: :up1_sum
-  text str: rolls2, layout: :up2_sum, markup: true
+  text str: data['Upgrade2'], layout: :upgrade2, range: only_lvl1_2
+  text str: data['Upgrade2'].map { |s| summarize_skill(s) }, layout: :up1_sum, range: only_lvl1_2
+  text str: rolls2, layout: :up2_sum, markup: true, range: only_lvl1_2 do |embed|
+    embed_emojis(embed, 30)
+  end
 
   save_png prefix: 'skill_back_', rotate: true#, range: 0
 
@@ -62,7 +68,6 @@ Squib::Deck.new(cards: total, width: 1125, height: 825) do
     save_sheet prefix: 'sheet_skills_backs_', columns: 5, rows: 5
   end
 
-  only_lvl1_2 = data['Level'].map.with_index { |x,i| [1].include?(x.to_i) ? i : nil }.compact
   save_pdf file: 'skill_backs.pdf', trim: '0.125in', range: only_lvl1_2
 
   build :color do
