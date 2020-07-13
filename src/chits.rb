97ceaaf20af590bf935a446391c931446138e8d0
Andy Meneely <andy@se.rit.edu>
2020-07-12 23:34:55 -0400

    chits.rb builds figures for rules

--- src/chits.rb
+++ src/chits.rb
@@ -1,13 +1,18 @@
 # Build chits for TTS
 require 'squib'
 
-bg_color = '#6b471c' # dark brown
-fg_color = '#bcbda7' # cream
+$BG_COLOR = '#6b471c' # dark brown
+$FG_COLOR = '#bcbda7' # cream
 
+def replace_fill_with_fg(svgfiles)
+  svgfiles.map do |f|
+    File.read("img/#{f}.svg").gsub('fill:#000000', "fill:#{$FG_COLOR}")
+  end
+end
 
 # Circle & Entrance Chits
 puts "Circle chits..."
-files = %w(
+circle_files = %w(
   guard
   subdued
   camera
@@ -21,26 +26,47 @@ files = %w(
   asterisk
   jewel
   folder
-  entrance
 )
+
+files = []
+files << circle_files
+files << 'entrance'
+files.flatten!
+
+## For TTS
 Squib::Deck.new(width: 300, height: 300, cards: files.size) do
-  background color: bg_color
-  svgdata = files.map do |f|
-    File.read("img/#{f}.svg").gsub('fill:#000000', "fill:#{fg_color}")  # cream color
-  end
-  svg data: svgdata, width: 300, height: 300
+  background color: $BG_COLOR
+  svg data: replace_fill_with_fg(files), width: 300, height: 300
   save_png prefix: 'chit_', count_format: files
 end
 
-# Money chits
+## For Rules
+Squib::Deck.new(width: 300, height: 300, cards: circle_files.size) do
+  circle x: 158, y: 158, radius: 140, fill_color: :black, stroke_width: 0
+  circle x: 150, y: 150, radius: 140, fill_color: $BG_COLOR, stroke_width: 0
+  svg data: replace_fill_with_fg(circle_files), width: 300, height: 300
+  save_png dir: 'rules/', prefix: 'fig_chit_', count_format: circle_files
+end
+
+# Money chits for TTS
 puts "Money chits..."
 Squib::Deck.new(width: 250, height: 135, cards: 3) do
-  background color: bg_color # dark brown
+  background color: $BG_COLOR # dark brown
   amounts = %w($1k $2k $5k)
-  text str: amounts, font: 'Archivo Narrow 24', color: fg_color,
+  text str: amounts, font: 'Archivo Narrow 24', color: $FG_COLOR,
        width: width, height: height, align: :center, valign: :middle
 
   save_png prefix: 'chit_', count_format: amounts.map { |x| x.gsub('$','') }
+end
+
+# Money chits for rules
+puts "Money chits..."
+Squib::Deck.new(width: 250, height: 135, cards: 3) do
+  amounts = %w($1k $2k $5k)
+  text str: amounts, font: 'Archivo Narrow 24', color: $FG_COLOR,
+       width: width, height: height, align: :center, valign: :middle
+
+  save_png dir: 'rules/', prefix: 'fig_chit_', count_format: amounts.map { |x| x.gsub('$','') }
 end
 
 # Hex chits
@@ -53,21 +79,30 @@ files = %w(
   watchtower
   keycard-lock
 )
+# For TTS
 Squib::Deck.new(width: 600, height: 600, cards: files.size) do
-  background color: bg_color # dark brown
-  svgdata = files.map do |f|
-    File.read("img/#{f}.svg").gsub('fill:#000000', "fill:#{fg_color}")  # cream color
-  end
-  svg data: svgdata, width: width, height: width
+  background color: $BG_COLOR # dark brown
+  svg data: replace_fill_with_fg(files), width: width, height: width
   save_png prefix: 'hex_', count_format: files
 end
+#For rules
+Squib::Deck.new(width: 600, height: 600, cards: files.size) do
+  polygon n: 6, x: 308, y: 310, radius: 290, angle: Math::PI / 6,
+          fill_color: :black, stroke_width: 0
+  polygon n: 6, x: 300, y: 300, radius: 290, angle: Math::PI / 6,
+          fill_color: $BG_COLOR, stroke_width: 0
+  svg data: replace_fill_with_fg(files), width: width, height: width
+  save_png dir: 'rules/', prefix: 'fig_hex_', count_format: files
+end
+
+
 
 # Entrance/Exit
 # files = %w(
 #   entrance
 # )
 # Squib::Deck.new(width: 300, height: 300, cards: file.size) do
-#   background color: bg_color # dark brown
+#   background color: $BG_COLOR # dark brown
 #
 #   save_png prefix: 'chit_', count_format: amounts.map { |x| x.gsub('$','') }
 # end
