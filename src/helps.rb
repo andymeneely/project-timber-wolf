ed47d8e1666af39bc440186d4b58e9182b98a141
Andy Meneely <andy@se.rit.edu>
2020-07-14 23:14:07 -0400

    player aids! finally a color version

--- src/helps.rb
+++ src/helps.rb
@@ -2,30 +2,75 @@
 require 'squib'
 require_relative 'util/helpers'
 
-data = Squib.xlsx file: 'data/data.xlsx', sheet: 5
+data = Squib.xlsx file: 'data/data.xlsx', sheet: 5 do |col, item|
+  escape_emojis(newlineate(col, item))
+end
 # combine the entire text into one string - not using row-per card here
 str = data['Icon'].zip(data['Description']).inject("") do |s, r|
   s + "#{r[0]} #{r[1]}\n"
 end
 
+
 Squib::Deck.new(cards: 4, width: 1125, height: 825) do
   use_layout file: 'layouts/helps.yml'
-  background color: :white
-  rect layout: :cut
+  png file: 'cork.png'
 
-  text layout: 'Description', str: str
+  svg file: 'help-color.svg'
 
-  save_png prefix: 'helps_'
-  save_pdf file: 'helps.pdf', trim: '0.125in'
+  text layout: :subactions, str: str do |embed|
+    embed_emojis(embed, 45)
+  end
+
+  ideas_str = <<~EOS
+    :idea:: change die +1/-1, wraparound
+    :idea:: 1 Escape Move at the end
+  EOS
+  text layout: :ideas, str: ideas_str do |embed|
+    embed_emojis(embed, 45)
+  end
+
+  text layout: :die_roll_1, str: 'Any die roll'
+  text layout: :die_roll_2, str: 'Die roll of 6'
+
+  save_png prefix: 'help_front_'
 end
 
-Squib::Deck.new(cards: 2, width: 1725, height: 1125) do
+
+Squib::Deck.new(cards: 4, width: 1125, height: 825) do
   use_layout file: 'layouts/helps.yml'
-  background color: :white
+  png file: 'cork.png'
+  svg file: 'help-color-back.svg'
 
-  svg file: ['noise_tracker_3p_card.svg',
-             'noise_tracker_4p_card.svg']
+  text layout: :planning, str: <<~EOS
+   - Place planning tokens
+   - Purchase items
+   - Use Fixer abilities
+  EOS
 
-  save_pdf file: 'noise_tracker.pdf'
+  text layout: :heist, str: <<~EOS
+  1. Roll dice
+  2. Action Phase, simultaneous play
+  3. Event phase
+     a. Do ONE event, then
+     b. Do ALL crises encountered
+  4. Repeat until Escape initiated
+  EOS
 
+  escape_str = <<~EOS
+  - Ignore planning tokens, cameras
+  - Each Escape Move costs an :idea:
+  - Cannot :unlock:
+  - Need :idea: to escape a Guard
+  - AFTER Escape Moves, may roll
+  EOS
+
+  text layout: :escape, str: escape_str do |embed|
+    embed_emojis(embed, 45)
+  end
+
+  text layout: :level2, str: 'Level 1 to 2: Upgrade Skill'
+  text layout: :level3, str: 'Level 2 to 3: Upgrade Char.'
+  text layout: :level4, str: 'Level 3 to 4: Upgrade Skill'
+
+  save_png prefix: 'help_back_'
 end
