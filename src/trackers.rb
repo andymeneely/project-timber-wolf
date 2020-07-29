f72142af6969c90f2c4e13f5ead74b431699e6ef
Andy Meneely <andy@se.rit.edu>
2020-07-29 12:29:36 -0400

    new event mat + noise tracker

--- src/trackers.rb
+++ src/trackers.rb
@@ -3,9 +3,15 @@
 require 'squib'
 require_relative 'util/helpers'
 
-Squib::Deck.new(cards: 2, width: 1125, height: 825) do
-  svg file: ['noise tracker 3p.svg', 'noise tracker 4p.svg']
+Squib::Deck.new(cards: 2, width: 3300, height: 2550) do
+  svg file: 'event mat.svg', width: width, height: height
+  svg file: ['noise tracker 3p.svg', 'noise tracker 4p.svg'],
+      x: 75, y: 1875, width: 3150, height: :scale
   save_png prefix: 'tracker_'
-  save_png dir: 'rules', prefix: 'fig_noise', count_format: '', range: 0,
-           trim: 75, trim_radius: 75
+end
+
+Squib::Deck.new(cards: 1, width: 600, height: 250) do
+  background color: :white
+  svg file: 'noise tracker 4p.svg', width: width * 0.95, height: :scale
+  save_png dir: 'rules', prefix: 'fig_noise', count_format: '', range: 0
 end
