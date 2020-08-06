5fc40bbb914dbf9e934040530087c0ca5f8fd9ce
Andy Meneely <andy@se.rit.edu>
2020-08-05 20:45:12 -0400

    new noise trackers, using MATHS

--- src/trackers.rb
+++ src/trackers.rb
@@ -6,7 +6,7 @@ require_relative 'util/helpers'
 Squib::Deck.new(cards: 2, width: 3300, height: 2550) do
   svg file: 'event mat.svg', width: width, height: height
   svg file: ['noise tracker 3p.svg', 'noise tracker 4p.svg'],
-      x: 75, y: 1875, width: 3150, height: :scale
+      x: 75, y: 1650, width: 3150, height: :scale
   save_png prefix: 'tracker_'
 end
 
